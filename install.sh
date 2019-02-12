cccec6b6888e3141615bea18fdf0eb5b2233d1de
Narui Masato <masato@Masato-MBP.local>
2019-02-12 23:05:16 +0900

    更新

--- install.sh
+++ install.sh
@@ -54,6 +54,19 @@ initalize(){
 configure(){
 	printf "\e[1;34m=== Configure Applications ===\e[0m \n"
 
+	# zsh
+	if [ "$(uname)" == "Darwin" ]; then
+		chsh -s /bin/zsh
+	elif [ "$(uname)" == "Linux" ]; then
+		chsh /bin/zsh
+	else
+		printf "\e[1;31m Do not support \e[0m \n"
+		exit 1
+	fi
+
+	# シェルの再起動
+	exec $SHELL -l
+
 	# anyenv
 	anyenv install --init
 
@@ -67,4 +80,4 @@ configure(){
 # main
 deploy
 initalize
-#configure
+configure
