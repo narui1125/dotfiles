d10ad03e79f91258999a5c9fa57e138c5a44777e
19M31275 <19M31275@pcm084044.edu2019.titech.ac.jp>
2019-04-25 08:53:19 +0900

    Update vim.sh

--- bin/vim.sh
+++ bin/vim.sh
@@ -4,8 +4,8 @@ vim_url="https://github.com/vim/vim.git"
 
 # インストール
 install_vim(){
-  if type "tmux" > /dev/null 2>&1 ; then
-    echo "Already installed tmux ✔︎"
+  if type "vim" > /dev/null 2>&1 ; then
+    echo "Already installed vim ✔︎"
   else
     printf "\e[1;34mInstall vim\e[0m\n"
 
