e0a5e2160110959e474ffb0104587fa9cd690124
Narui Masato <masato@Masato-MBP.local>
2019-04-28 09:50:49 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,14 +38,19 @@ deploy(){
 						printf "install docker"
 
 						# docker 
-						sudo yum install -y yum-utils \
-							device-mapper-persistent-data \
-							lvm2
-						sudo yum-config-manager \
-							--add-repo \
-							https://download.docker.com/linux/centos/docker-ce.repo
-						sudo yum install docker-ce docker-ce-cli containerd.io
-						sudo systemctl start docker
+						sudo apt-get install \
+    						apt-transport-https \
+							ca-certificates \
+							curl \
+							gnupg-agent \
+							software-properties-common
+						curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+						sudo add-apt-repository \
+							"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+							$(lsb_release -cs) \
+							stable"
+						sudo apt-get update
+						sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 						# docker-compose
 						source "${BIN_DIR}/docker.sh"
