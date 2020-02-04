088bfc3967fb0f6c34a942ff919e415cea986b84
Narui Masato <narui1125@users.noreply.github.com>
2020-02-04 16:09:06 +0900

    Update .p10k.zsh

--- home/.p10k.zsh
+++ home/.p10k.zsh
@@ -681,9 +681,9 @@
   typeset -g POWERLEVEL9K_CONTEXT_BACKGROUND=15
 
   # Context format when running with privileges: user@hostname.
-  typeset -g POWERLEVEL9K_CONTEXT_ROOT_TEMPLATE='%%m'
+  typeset -g POWERLEVEL9K_CONTEXT_ROOT_TEMPLATE='%m'
   # Context format when in SSH without privileges: user@hostname.
-  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_TEMPLATE='%%m'
+  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_TEMPLATE='%m'
   # Default context format (no privileges, no SSH): user@hostname.
   typeset -g POWERLEVEL9K_CONTEXT_TEMPLATE='%m'
 
