be1a8034dbea79f0ac7901e2b0cd66e69e29b082
政人 <masato@Masato-Lab-MBP.local>
2020-11-14 19:02:04 +0900

    Update

--- bin/homebrew.sh
+++ bin/homebrew.sh
@@ -4,19 +4,10 @@
 # インストール
 install_homebrew(){
   if type "brew" > /dev/null 2>&1 ; then
-    echo "Already installed Homebrew ✔︎"
+    printf "\e[1;32mAlready installed Homebrew ✔︎\e[0m\n"
   else
     printf "\e[1;34mInstall homebrew\e[0m\n"
 
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" < /dev/null
-  fi
-}
-
-# 設定
-configure_homebrew(){
-  if type "brew" > /dev/null 2>&1 ; then
-    printf "\e[1;34mConfigure homebrew\e[0m\n"
-  else
-    echo "Not installed homebrew. Skipped ✔︎"
   fi
 }
