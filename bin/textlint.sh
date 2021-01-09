1bf1d5238769233ca64c3a6270fda614883d5793
Masato Narui <narui.biz@icloud.com>
2021-01-09 17:37:28 +0900

    Upd

--- bin/textlint.sh
+++ bin/textlint.sh
@@ -9,6 +9,7 @@ install_textlint(){
     printf "\e[1;34mInstall textlint\e[0m\n"
     npm install --global textlint
     npm install --global textlint-plugin-latex2e
+    npm install --global textlint-filter-rule-comments
     npm install --global \
         textlint-rule-preset-ja-technical-writing \
         textlint-rule-preset-jtf-style \
