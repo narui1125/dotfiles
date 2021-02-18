082714d2470f271a5ec4619d3fca6d0e0b7d7924
Masato Narui <narui.biz@icloud.com>
2021-02-18 19:10:20 +0900

    Upd

--- install.sh
+++ install.sh
@@ -7,18 +7,12 @@ SCRIPT_DIR=$(cd $(dirname $0);pwd)
 
 # ダウンロード
 download(){
-	printf "\e[1;34m=== Download dotfiles ===\e[0m \n"
-	
-	if [ -d "${DOTFILES_DIR}" ]; then
-		printf "${DOTFILES_DIR} already exists \n"
-		return
-    fi
-
 	if [ "$(uname)" == "Darwin" ]; then
 		xcode-select --install
 	fi
 
 	if type "git" > /dev/null 2>&1 ; then
+		printf "Download dotfiles...\n"
 		git clone --recursive "${DOTFILES_REPOSITORY}" "${DOTFILES_DIR}"
 	else
 		printf "git not found"
@@ -29,6 +23,9 @@ download(){
 # 設定ファイルの展開
 deploy(){
 	printf "\e[1;34m=== Create dotfile symbolic links ===\e[0m \n"
+
+	# ダウンロード
+	[ ! -d "${DOTFILES_DIR}" ] && download
 
 	cd "${DOTFILES_DIR}/home"
 	for f in .??*
@@ -46,7 +43,11 @@ deploy(){
 initialize(){
 	printf "\e[1;34m=== Install Applications ===\e[0m \n"
 
-	if !["$(uname)" == "Darwin" || "$(uname)" == "Linux"]; then
+	# ダウンロード
+	[ ! -d "${DOTFILES_DIR}" ] && download
+
+	# OSチェック
+	if [ ! "$(uname)" == "Darwin" ] && [ ! "$(uname)" == "Linux" ]; then
 		printf "not Darwin or Linux\n"
 		exit 1
 	fi
@@ -54,15 +55,13 @@ initialize(){
 	# Homebrew
 	source "${DOTFILES_DIR}/bin/homebrew.sh" && install_homebrew
 	
+	# Homebrew Applications
 	eval $($HOME/.linuxbrew/bin/brew shellenv)
 	brew update && brew bundle --file="${DOTFILES_DIR}/etc/Brewfile"
 }
 
 # main
 install(){
-	download
 	deploy
 	initialize
 }
-
-install
