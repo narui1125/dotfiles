3af06da091b877310bd9926327f6cd2339d1caa3
narui1125@users.noreply.github.com <Narui Masato>
2021-02-20 19:48:10 +0900

    upd

--- bin/darwin.sh
+++ bin/darwin.sh
@@ -71,4 +71,4 @@ defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 
 # ===== iTerm2 =====
 
-defaults write com. googlecode.iterm2.plist PrefsCustomFolder -string "${DOTFILES_DIR}/etc/iterm2"
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "${DOTFILES_DIR}/etc/iterm2"
